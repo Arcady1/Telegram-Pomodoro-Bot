2f9a752d70a8600a4ce97a77857152a4b280e5fe
Arcady1 <insite1029@gmail.com>
2020-06-10 16:50:24 +0300

    to-deploy-Glitch_v2

--- project_modules/messages.js
+++ project_modules/messages.js
@@ -6,7 +6,7 @@ function fullRules(typeOfRule, title = '') {
     rulText = 'This bot will write you,\nwhen you have to work and relax.\nPress START for detals.';
 
   else if (typeOfRule == 'butStart')
-    rulText = 'Send me two numbers, for example: 50 10.\nI\'ll notify you after 50 and 60 minutes\n==================\nMinimum: 1min\nMaximum: 1439min (23h 59min)\n==================\nIt\'ll be repeated until then you press STOP.';
+    rulText = 'Send me two numbers, for example: 50 10.\nI\'ll notify you after 50 and 60 minutes\n\nMinimum: 1min\nMaximum: 1439min (23h 59min)\n\nIt\'ll be repeated until then you press STOP.';
   else if (typeOfRule == 'butStop')
     rulText = 'Press START to restart me and find out some details.';
   else if (typeOfRule == 'butHelp')
