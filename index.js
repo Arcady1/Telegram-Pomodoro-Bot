021139075bb7431776d98dbf8aead2aef020aedd
Arcady1 <insite1029@gmail.com>
2020-06-12 20:27:46 +0300

    bebug

--- index.js
+++ index.js
@@ -11,24 +11,98 @@ const bot = new TelegramBot(token, {
 });
 messages.setBot(bot); // добавляем bot в модуль messages, чтобы оттуда отправлять сообщения
 
-// при старте бота
-bot.onText(/\/start/, msg => {
+bot.on('message', msg => {
+  let isCommand = false;
   let userId = msg.from.id; // id отправителя сообщения 
-  // появление клавиатуры
-  bot.sendMessage(userId, ('Hello, ' + msg.from.first_name + '!\n' + messages.botAnswers('firstStart')), {
-    reply_markup: {
-      keyboard: myKeyboard.startKb
-    }
-  });
+  let userText = msg.text; // текст отправителя 
+  let workRealxRegExp = /(\d\d*) (\d\d*)/; // ожидаемые интервалы работы / отдыха; хранятся в match[1] и match[3] 
+  let wrongTimeRegExp = /(\d\d):(\d)(\d)/; // время пользователя
+
+  messages.setUserID(userId); // добавляем userId в модуль messages, чтобы оттуда отправлять сообщения
+
+  switch (msg.text) {
+    case '/start':
+      // появление клавиатуры
+      bot.sendMessage(userId, ('Hello, ' + msg.from.first_name + '!\n' + messages.botAnswers('firstStart')), {
+        reply_markup: {
+          keyboard: myKeyboard.startKb
+        }
+      });
+      isCommand = true;
+      break;
+    case 'START':
+      // сброс таймера
+      botFunctions.clrTimeout();
+      // появление клавиатуры
+      bot.sendMessage(userId, messages.botAnswers('butStart'), {
+        reply_markup: {
+          keyboard: myKeyboard.intervalsKb
+        }
+      });
+      isCommand = true;
+      break;
+    case 'WRONG TIME':
+      // сброс таймера
+      botFunctions.clrTimeout();
+      // появление клавиатуры
+      bot.sendMessage(userId, 'Send me the correct time (for example 15:30):', {
+        reply_markup: {
+          keyboard: myKeyboard.stopOnlyKb
+        }
+      });
+      isCommand = true;
+      break;
+    case 'STOP':
+      // сброс таймера
+      botFunctions.clrTimeout();
+      // появление клавиатуры
+      bot.sendMessage(userId, messages.botAnswers('butStop'), {
+        reply_markup: {
+          keyboard: myKeyboard.startKb
+        }
+      });
+      isCommand = true;
+      break;
+    case 'PAUSE':
+      // сброс таймера
+      botFunctions.clrTimeout();
+      // появление кнопки RESUME
+      bot.sendMessage(userId, messages.botAnswers('butPause', botFunctions.timeLeft()), {
+        reply_markup: {
+          keyboard: myKeyboard.pauseKb
+        }
+      });
+      isCommand = true;
+      break;
+    case 'RESUME':
+      // появление кнопки PAUSE
+      bot.sendMessage(userId, 'I\'m working again', {
+        reply_markup: {
+          keyboard: myKeyboard.stopKb
+        }
+      }).then(() => {
+        botFunctions.countdown(bot, note, botFunctions.timeLeft(true)); // последний аргумент - оставшееся время до уведомления (в min) 
+      })
+      isCommand = true;
+      break;
+    case '/help':
+      messages.botSendMyMessage('butHelp');
+      isCommand = true;
+      break;
+  }
+
+  if (isCommand == false) {
+    // ! /help
+  }
 })
+
+
 
 // при вводе команд
 bot.onText(/\/?(\w+)(\s)*(\w+)*/, (msg, match) => {
-  let userId = msg.from.id; // id отправителя сообщения 
-  messages.setUserID(userId); // добавляем userId в модуль messages, чтобы оттуда отправлять сообщения
-  let userText = msg.text; // текст отправителя 
-  let workRealxRegExp = /(\d\d*) (\d\d*)/; // ожидаемые интервалы работы / отдыха; хранятся в match[1] и match[3] 
-  let wrongTimeRegExp = /(\d)(\d)\:(\d)(\d)/; // время пользователя
+
+
+  // !!!
   let work = match[1];
   let relax = match[3];
   let note = {
@@ -37,16 +111,8 @@ bot.onText(/\/?(\w+)(\s)*(\w+)*/, (msg, match) => {
     'relaxTime': parseInt(relax)
   }
   console.log(match[0]);
-  if (userText == 'START') {
-    // сброс таймера
-    botFunctions.clrTimeout();
-    // появление клавиатуры
-    bot.sendMessage(userId, messages.botAnswers('butStart'), {
-      reply_markup: {
-        keyboard: myKeyboard.intervalsKb
-      }
-    });
-  }
+
+
   // интервал работы - отдыха
   else if (workRealxRegExp.test(match[0])) {
     // сброс таймера, чтобы интервалы не накладывались 
@@ -54,61 +120,12 @@ bot.onText(/\/?(\w+)(\s)*(\w+)*/, (msg, match) => {
     // отсчет до уведомления
     botFunctions.countdown(bot, note);
   }
-  // WRONG TIME 
-  else if (userText == 'WRONG TIME') {
-    // сброс таймера
-    botFunctions.clrTimeout();
-    bot.sendMessage(userId, 'Send me the correct time (for example 15:30):', {
-      reply_markup: {
-        keyboard: myKeyboard.stopOnlyKb
-      }
-    });
-  }
+
   // указано время 
   // ! НЕТ СООТВЕТСВИЯ REGEXP  в 26 строке
-  
   else if (wrongTimeRegExp.test(match[0])) {
     console.log("Yes!");
   }
-  // STOP
-  else if (userText == 'STOP') {
-    // сброс таймера
-    botFunctions.clrTimeout();
-    // появление клавиатуры
-    bot.sendMessage(userId, messages.botAnswers('butStop'), {
-      reply_markup: {
-        keyboard: myKeyboard.startKb
-      }
-    });
-  }
-  // PAUSE
-  else if (userText == 'PAUSE') {
-    // сброс таймера
-    botFunctions.clrTimeout();
-    // появление кнопки RESUME
-    bot.sendMessage(userId, messages.botAnswers('butPause', botFunctions.timeLeft()), {
-      reply_markup: {
-        keyboard: myKeyboard.pauseKb
-      }
-    });
-  }
-  // RESUME
-  else if (userText == 'RESUME') {
-    // появление кнопки PAUSE
-    bot.sendMessage(userId, 'I\'m working again', {
-      reply_markup: {
-        keyboard: myKeyboard.stopKb
-      }
-    }).then(() => {
-      botFunctions.countdown(bot, note, botFunctions.timeLeft(true)); // последний аргумент - оставшееся время до уведомления (в min) 
-    })
-  }
-  // /help
-  else if (userText == '/help') {
-    messages.botSendMyMessage('butHelp');
-  }
-  // если команды не существует и не является /start
-  else if (userText != '/start') {
-    messages.botSendMyMessage('butHelp');
-  }
+
+
 })
