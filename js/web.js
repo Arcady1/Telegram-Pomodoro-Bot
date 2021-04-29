60961030afacdb1d8c5bba71c4e05d9caeea322a
Arkady1 <insite1029@gmail.com>
2021-04-29 12:47:53 +0300

    deploy

--- js/web.js
+++ js/web.js
@@ -2,17 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// var packageInfo = require('./package.json');
+// Port
+app.set('port', PORT);
 
-// app.get('/', function (req, res) {
-//     res.json({
-//         version: packageInfo.version
-//     });
-// });
+app.get('/', function (req, res) {
+    res.json({
+        version: '1.0.0'
+    });
+});
 
-var server = app.listen(PORT, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log(`Web server started at http://${host}:${port}`);
+app.listen(PORT, () => {
+    console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
